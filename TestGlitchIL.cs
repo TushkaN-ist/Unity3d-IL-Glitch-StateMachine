e01fa05b14ce056f836d88b11ad8f88a8397002e
TushkaN <tushkan.boi.fas@gmail.com>
2024-05-08 03:24:01 +0400

    glitch test
    glitched component

---
+++ TestGlitchIL.cs
@@ -0,0 +1,42 @@
+using CustomVR;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class TestGlitchIL : MonoBehaviour, ITestGlitch
+{
+	public IEnumerable<IHandGrab> Grabs => throw new System.NotImplementedException();
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			((ITestGlitch)this).Grabbed(true, null);
+		}
+	}
+	public void Grabbed(bool grab, params int[] inputs)
+	{
+		Debug.Log("Grab");
+	}
+	public void UnGrabbed(bool grab, params int[] inputs)
+	{
+		Debug.Log("Ungrab");
+	}
+}
+public interface ITestGlitch
+{
+	public static Dictionary<GameObject, ITestGlitch> grabbed = new Dictionary<GameObject, ITestGlitch>();
+	public static bool TryGetValue(GameObject obj, out ITestGlitch handGrabbed)
+	{
+		return grabbed.TryGetValue(obj, out handGrabbed);
+	}
+	public static bool TryGetValue<T>(GameObject obj, out T handGrabbed) where T : ITestGlitch
+	{
+		ITestGlitch result;
+		handGrabbed = default(T);
+		return TryGetValue(obj, out result) && (result is T && (handGrabbed = (T)result) != null);
+	}
+	public void Grabbed(bool grab, params int[] inputs);
+	public void UnGrabbed(bool grab, params int[] inputs);
+}
+
